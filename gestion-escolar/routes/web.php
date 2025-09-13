d7b6dfb94db644eb6b431e0a31aa7183e7016970
Nicko <nicolas2014velasquez@gmail.com>
2025-09-13 19:03:29 -0300

    Descarte De Pruebas
    Punto De Partida_ + Migraciones - Tablas

--- gestion-escolar/routes/web.php
+++ gestion-escolar/routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('saludito');
+    return view('welcome');
 });
 
-Route::get('/despedida', function () {
-    return view('despedida');
-});
